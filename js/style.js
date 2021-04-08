7ea459dcca0692d30dd9d84033d0351a1fc39722
hinakot1228 <74905456+hinakot1228@users.noreply.github.com>
2021-04-08 16:58:36 +0900

    Update style.js

--- js/style.js
+++ js/style.js
@@ -1,44 +1,76 @@
 /*---------------------------------
   PC画面のサイドバーのアコーディオンメニュー
 ---------------------------------*/
-var btn = document.getElementsByClassName("btn");
-var i;
-for(i = 0; i < btn.length; i++) {
-  btn[i].addEventListener("click",function(){
-    var panel = this.nextElementSibling;
-    if(panel.style.display == "block") {
-      $(panel).slideUp(300);
-    }else {
-      $(panel).slideDown(300);
+$(function() {
+  $('.side-bar-btn').click(function()  {
+    var category = $(this).hasClass('category-btn');
+    if(category){
+      $(this).nextAll('.panel').slideToggle();
     }
-  })
-}
 
-// $(function(){
-// 	$('.toggle_title').click(function(){
-// 		$(this).toggleClass('selected');
-// 		$(this).next().slideToggle();
-// 	});
-// });
+    
+  });
+});
+// var btn = document.getElementsByClassName("btn");
+// var i;
+// for(i = 0; i < btn.length; i++) {
+//   btn[i].addEventListener("click",function(){
+//     var panel = this.nextElementSibling;
+//     if(panel.style.display == "block") {
+//       $(panel).slideUp(300);
+//     }else {
+//       $(panel).slideDown(300);
+//     }
+//   })
+// }
 
-// $(function(){
-//   $('.single-item').slick({
-//     // accessibility: true,
-//     autoplay: false,
-//     autoplaySpeed: 1000,
-//     dots: true,
-//     // fade: true,
-//     slidesToShow:4,
-//     slidesToScroll:4
-//   });
-// });
+$(function(){
+	$('.toggle_title').click(function(){
+		$(this).toggleClass('selected');
+		$(this).next().slideToggle();
+	});
+});
 
-// $(function(){
-// 	$('.toggle_title').click(function(){
-// 		$(this).toggleClass('selected');
-// 		$(this).next().slideToggle();
-// 	});
-// });
+$(function(){
+  $('.single-item').slick({
+    // accessibility: true,
+    autoplay: false,
+    autoplaySpeed: 1000,
+    dots: true,
+    // fade: true,
+    slidesToShow:4,
+    slidesToScroll:4,
+    arrows: true,
+
+    responsive: [{
+      breakpoint: 480,  //ブレイクポイントを指定
+      settings: {
+        slidesToShow:2.5,
+        slidesToScroll:2.5,
+        dots: false,
+        arrows: false,
+      }
+    }]
+
+  });
+});
+
+$(function(){
+	$('.toggle_title').click(function(){
+		$(this).toggleClass('selected');
+		$(this).next().slideToggle();
+	});
+});
+
+var $slider_container = $('.container'),
+    $slider = $('.single-item');
+
+$slider.slick({
+  appendArrows: $slider_container,
+  // FontAwesomeのクラスを追加
+  prevArrow: '<div class="slider-arrow slider-prev fa fa-angle-left"></div>',
+  nextArrow: '<div class="slider-arrow slider-next fa fa-angle-right"></div>',
+});
 
 /*---------------------------------
   モバイル画面のヘッダーのハンバーガーメニュー
